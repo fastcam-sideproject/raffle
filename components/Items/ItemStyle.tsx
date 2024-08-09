d5c3d03552d38d77c1939af9ece33002e67ef514
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-08-10 01:52:10 +0900

    fix : 태그 수정

--- components/Items/ItemStyle.tsx
+++ components/Items/ItemStyle.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
-import { useAuthStore } from '../../lib/store/useAuthStore';
 import { useMutation } from '@tanstack/react-query';
+import { useAuthStore } from '../../lib/store/useAuthStore';
 import { postPurchaseRaffle } from '../../api/raffle/raffleApi';
 import { ItemProps } from '../../lib/store/item';
 
@@ -31,24 +31,27 @@ export default function ItemStyle({
   };
 
   return (
-    <div className="p-4 w-full flex flex-col gap-4">
+    <li
+      id={raffleId}
+      className="p-4 w-full flex flex-col gap-4 rounded shadow-custom-light"
+    >
       <Image
         priority
         width={100}
         height={100}
         src={imageUrl}
         alt="상품 이미지"
-        className="w-full"
+        className="w-full h-80 rounded object-contain"
       />
-      <div className="bg-gray-50 p-4 w-full">
+      <div className="bg-gray-50 p-4 w-full rounded">
         <div>
           <h4 className="text-lg font-bold">{name}</h4>
           <span className="text-gray-400">카테고리{category}</span>
         </div>
         <button
-          className={`mt-2 px-2 py-1 ${
-            percentageComplete === 100 ? 'bg-red-400' : 'bg-blue-400'
-          } text-white rounded float-right max-md:float-none max-md:w-full`}
+          type="button"
+          className={`mt-2 px-2 py-1 ${percentageComplete === 100 ? 'bg-red-400' : 'bg-blue-400'
+            } text-white rounded float-right max-md:float-none max-md:w-full`}
           onClick={handlePurchase}
         >
           {percentageComplete === 100 ? '결과확인' : '구매하기'}
@@ -57,15 +60,14 @@ export default function ItemStyle({
           <span className="text-lg font-semibold">{percentageComplete}%</span>
           <span>{percentageComplete === 100 ? '진행중' : '완료'}</span>
         </div>
-        <div className="mt-6 w-full  bg-gray-300 rounded-full h-3 overflow-hidden">
+        <div className="mt-6 w-full bg-gray-300 rounded-full h-3 overflow-hidden ">
           <div
-            className={`h-full transition-all duration-300 ${
-              percentageComplete === 100 ? 'bg-red-400' : 'bg-blue-400'
-            }`}
+            className={`h-full transition-all duration-300 ${percentageComplete === 100 ? 'bg-red-400' : 'bg-blue-400'
+              }`}
             style={{ width: `${percentageComplete}%` }}
           />
         </div>
       </div>
-    </div>
+    </li>
   );
 }
