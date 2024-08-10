a392329af27dde4edcc14502501fd0d66a4305a4
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-08-10 18:33:35 +0900

    fest : 광고 팝업 구현 / 높이 문제해결할것

--- components/Advertisement/AdvertisementStyle.tsx
+++ components/Advertisement/AdvertisementStyle.tsx
@@ -11,21 +11,33 @@ export default function AdvertisementStyle() {
   };
 
   return (
-    <main className="absolute z-50 bg-gray-600 w-full h-vh bg-opacity-40 flex items-center justify-center">
-      <section className="w-3/4 h-full bg-red-600">
+    <main
+      className={`${toggle ? 'hidden' : 'absolute top-0  z-50 bg-gray-600 w-full min-h-full bg-opacity-40 flex items-center justify-center'}`}
+    >
+      <section className="w-3/4 h-full mt-40 flex flex-col gap-6 justify-center items-center">
         <h2>광고타이틀</h2>
-        <div className="relative">
-          <Image width={100} height={100} src="" alt="광고이미지" className="w-full h-full" />
+        <div className="relative w-full">
+          <Image
+            width={1000}
+            height={1000}
+            src="/image/background/1.jpg"
+            alt="광고이미지"
+            className="w-full h-full"
+          />
           <button
             aria-label="광고닫기"
             type="button"
-            className="absolute top-0 right-4"
+            className="absolute top-4 right-4 bg-slate-50 p-4 rounded-lg"
             onClick={handleCloseButton}
           >
-            광고닫기
+            광고닫기 광고닫는 숫자들어감
           </button>
         </div>
-        <button aria-label="응모하기" type="button" className="bg-blue-400">
+        <button
+          aria-label="응모하기"
+          type="button"
+          className="bg-blue-400 py-4 px-8 rounded-2xl text-white"
+        >
           응모하기
         </button>
       </section>
