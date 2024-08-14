0ac63af79b992954242961ff73aeb5baa6cb2762
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-08-14 14:18:46 +0900

    refactor : 경로 오류/ 콘솔오류 수정

--- components/Header/HeaderNav.tsx
+++ components/Header/HeaderNav.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import GoogleLoginButton from '../GoogleLoginButton';
 import { useAuthStore } from '../../lib/store/useAuthStore';
 import ProfilePopover from '../ProfilePopover';
@@ -39,7 +39,13 @@ export default function HeaderNav() {
   return (
     <header className="w-full bg-slate-100 sticky top-0 border-solid border-b-1 border-indigo-900 shadow-custom-light">
       <nav className="flex justify-between items-center px-6 py-2">
-        <Image width={100} height={100} src="/image/logo/logo_title.png" alt="Logo_img" />
+        <Image
+          width={100}
+          height={100}
+          src="/image/logo/logo_title.png"
+          alt="Logo_img"
+          className="w-auto h-auto"
+        />
         <ul
           className={`${
             toggle ? 'absolute w-full top-16 left-0 flex flex-col p-5 gap-1' : 'max-sm:hidden'
