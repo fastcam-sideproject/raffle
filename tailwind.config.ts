d57e7173841e1e26c50cc02496639b3ac918f942
wook <yho7955@naver.com>
2024-08-14 11:51:59 +0900

    chore: 색상 변수 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,10 +17,29 @@ const config: Config = {
           'rgb(255, 255, 255) 3px 0px 0px, rgb(255, 255, 255) 2.83487px 0.981584px 0px, rgb(255, 255, 255) 2.35766px 1.85511px 0px, rgb(255, 255, 255) 1.62091px 2.52441px 0px, rgb(255, 255, 255) 0.705713px 2.91581px 0px, rgb(255, 255, 255) -0.287171px 2.98622px 0px, rgb(255, 255, 255) -1.24844px 2.72789px 0px, rgb(255, 255, 255) -2.07227px 2.16926px 0px, rgb(255, 255, 255) -2.66798px 1.37182px 0px, rgb(255, 255, 255) -2.96998px 0.42336px 0px, rgb(255, 255, 255) -2.94502px -0.571704px 0px, rgb(255, 255, 255) -2.59586px -1.50383px 0px, rgb(255, 255, 255) -1.96093px -2.27041px 0px, rgb(255, 255, 255) -1.11013px -2.78704px 0px, rgb(255, 255, 255) -0.137119px -2.99686px 0px, rgb(255, 255, 255) 0.850987px -2.87677px 0px, rgb(255, 255, 255) 1.74541px -2.43999px 0px, rgb(255, 255, 255) 2.44769px -1.73459px 0px, rgb(255, 255, 255) 2.88051px -0.838247px 0px',
       },
     },
+
+    // colors: {
+    //   primary: '#60A5FA',
+    //   secondary: '#f87171',
+    //   error: '#dc2626',
+    //   white: '#ffffff',
+    //   black: '#000000',
+    //   gray: {
+    //     50: '#f9fafb',
+    //     100: '#f3f4f6',
+    //     200: '#e5e7eb',
+    //     300: '#d1d5db',
+    //     400: '#9ca3af',
+    //     500: '#6b7280',
+    //     600: '#4b5563',
+    //     700: '#374151',
+    //     800: '#1f2937',
+    //     900: '#111827',
+    //   },
+    // },
   },
   plugins: [
-    // eslint-disable-next-line func-names
-    function ({ addUtilities }) {
+    function ({ addUtilities }: { addUtilities: Function }) {
       const newUtilities = {
         '.text-shadow-white-shadow': {
           textShadow:
