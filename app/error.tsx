f9dc6391ffe67ec4ee9b177c7e0b4a10ddca8ec4
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-08-16 16:47:50 +0900

    feat : 에러/ 404 페이지 생성

---
+++ app/error.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import HeaderNav from '../components/Header/HeaderNav';
+
+function Error() {
+  return (
+    <>
+      <HeaderNav />
+      에러발생
+    </>
+  );
+}
+
+export default Error;
