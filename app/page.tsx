3ca5cf4bfead68c37b796bbc9a1fdcc6174b597d
wook <yho7955@naver.com>
2024-08-20 11:11:53 +0900

    refactor: localstorage를 sessionStorage로 변경

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,12 @@ export default function Home() {
     const url = new URL(window.location.href);
     const token = url.searchParams.get('access_token');
 
+    /**
+     * 만약 token이 있다면 localStorage에 저장하고 setUserToken을 호출하여 token을 저장.
+     * 그리고 router를 이용하여 홈으로 이동.
+     */
     if (token) {
-      localStorage.setItem('access_token', token);
+      sessionStorage.setItem('access_token', token);
       setUserToken(token);
       router.replace('/');
     }
--- lib/store/useAuthStore.ts
+++ lib/store/useAuthStore.ts
@@ -1,16 +1,23 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { createJSONStorage, persist } from 'zustand/middleware';
 import { AuthStore } from '../types/authStore';
 
 const useAuthStore = create<AuthStore>()(
   persist(
     (set) => ({
       userToken: '',
-      setUserToken: (userToken) => set({ userToken }),
-      logout: () => set({ userToken: '' }),
+      setUserToken: (userToken) => {
+        sessionStorage.setItem('access_token', userToken);
+        set({ userToken });
+      },
+      logout: () => {
+        sessionStorage.removeItem('access_token');
+        set({ userToken: '' });
+      },
     }),
     {
       name: 'auth-storage',
+      storage: createJSONStorage(() => sessionStorage),
     },
   ),
 );
