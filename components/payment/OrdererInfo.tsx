6fd89461f153106e980b4dda502b1318e7f52586
wook <yho7955@naver.com>
2024-08-20 14:44:14 +0900

    feat: 주문자 정보 컴포넌트에 로그인한 유저의 이름, 이메일 데이터 브라우저에 나타나록 구현

--- components/payment/OrdererInfo.tsx
+++ components/payment/OrdererInfo.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PhoneNumber from './PhoneNumber';
+import { useQuery } from '@tanstack/react-query';
+import { getMyPage } from '../../api/user/mypageApi';
+import useAuthStore from '../../lib/store/useAuthStore';
 
 export default function OrdererInfo() {
+  const userToken = useAuthStore((state) => state.userToken);
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['ordererInfo'],
+    queryFn: () => getMyPage(userToken),
+    staleTime: 1000 * 60 * 5,
+  });
+
+  if (!data || isError) {
+    return <p>주문자 정보를 불러오는데 실패했습니다.</p>;
+  }
+
+  if (isLoading) {
+    return <p>주문자 정보를 불러오는 중입니다.</p>;
+  }
+
   return (
     <section className="border p-4 rounded-md">
       <h2 className="text-xl font-semibold mb-4">주문자 정보</h2>
-      <div className="space-y-2">
-        <p>홍길동11</p>
-        <PhoneNumber />
+      <div className="space-y-2 text-lg">
+        <p>{data.name}</p>
+        <p>{data.email}</p>
+        {data.phoneNumber ? <p>{data.phoneNumber}</p> : <PhoneNumber />}
       </div>
     </section>
   );
