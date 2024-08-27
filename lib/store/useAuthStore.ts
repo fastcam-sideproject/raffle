3478877ca37fa1a03e719134da7f3e887908cab9
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-08-27 12:27:51 +0900

    remove : 린터 삭제 파일이름 변경

--- lib/store/useAuthStore.ts
+++ lib/store/useAuthStore.ts
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
-import { AuthStore } from '../types/authStore';
+import { AuthStore } from '../types/authStores';
+
 
 const useAuthStore = create<AuthStore>()(
   persist(
