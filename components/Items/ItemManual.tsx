5688f1aeae67139558060706cda65db8df438c58
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-08-29 20:14:34 +0900

    refactor : z-index 수정

--- components/Items/ItemManual.tsx
+++ components/Items/ItemManual.tsx
@@ -5,7 +5,7 @@ import { ItemManualProps } from '../../lib/types/item';
 
 export default function ItemManual({ onClose }: ItemManualProps) {
   return (
-    <main className="fixed inset-0 z-50 bg-gray-600 bg-opacity-40 flex flex-col items-center justify-center">
+    <main className="fixed inset-0 z-[1050] bg-gray-600 bg-opacity-40 flex flex-col items-center justify-center">
       <section className="w-11/12 md:w-4/5 lg:w-3/5 h-auto max-h-[90%] flex flex-col gap-6 justify-center items-center bg-white rounded-lg p-4 md:p-6">
         <h2 className="text-lg md:text-xl font-bold text-center text-gray-500">래플 이용 방법</h2>
         <div className="w-full flex flex-col gap-2">
