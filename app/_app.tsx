401174018e214409d91e94308fe9cecda9774b1e
van1164 <vkn123@naver.com>
2024-08-30 01:51:06 +0900

    feat: google ad script추가

---
+++ app/_app.tsx
@@ -0,0 +1,7 @@
+import Script from 'next/script';
+
+<Script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2820184336962070"
+    crossOrigin="anonymous"
+/>
