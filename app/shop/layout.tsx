ef670b09dbf3bc2bfb2894e4737a0616e04517da
wook <yho7955@naver.com>
2024-09-02 15:53:08 +0900

    refactor: layout 이름 변경

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -1,7 +1,7 @@
 import Banner from '../../components/Banner/Banner';
 import HeaderNav from '../../components/Header/HeaderNav';
 
-export default function AboutLayout({ children }: { children: React.ReactNode }) {
+export default function ShopLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <HeaderNav />
