b9e0e8b1093a84aaed615c1f18e8cd100ae9187b
wook <yho7955@naver.com>
2024-09-03 12:27:54 +0900

    refactor: 불필요한 import문 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import HomeMain from '../components/Home/HomeMain';
 import HeaderNav from '../components/Header/HeaderNav';
 import AuthHandler from '../components/AuthHandler';
-import Advertisement from '../components/Advertisement/Advertisement';
 
 export default function HomePage() {
   return (
