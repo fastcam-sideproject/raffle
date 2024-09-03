ba76d45ff13e6810c094d33a45c7896a9957ee53
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-09-03 18:52:49 +0900

    refactor : 메인 광고 삭제

--- components/Home/HomeMain.tsx
+++ components/Home/HomeMain.tsx
@@ -9,9 +9,6 @@ export default function HomeMain() {
   return (
     <main>
       <HomeHero />
-      <section className="w-full flex justify-center my-6">
-        <KakaoAdFit unit="DAN-qvc3rvDUKVUobMDZ" width="300" height="250" disabled={false} />
-      </section>
       <ItemPopular />
       <HomeInfo />
     </main>
