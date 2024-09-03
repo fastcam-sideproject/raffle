e78433e52e8d9afda260eb14bd9dd087c40a706f
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-09-03 18:44:22 +0900

    style : 구글 버튼 수정

--- components/Button/GoogleLoginButton.tsx
+++ components/Button/GoogleLoginButton.tsx
@@ -15,7 +15,7 @@ export default function GoogleLoginButton() {
       onClick={handleLogin}
       className="flex items-center text-lg  border py-2 px-4 gap-2 bg-white text-black rounded hover:bg-gray-100 transition-colors"
     >
-      <Image width={200} height={200} src="/image/logo/google_logo.svg" alt="구글 로그인 아이콘" />
+      <Image width={200} height={200} src="/image/logo/google_logo.svg" alt="구글 로그인 아이콘" className='w-[20px] h-[20px]'/>
       <span className="max-sm:hidden">Sign in with Google</span>
     </button>
   );
