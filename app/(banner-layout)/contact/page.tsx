a319e876cb14d597f6d565b13723d5460bb475c1
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-09-04 17:07:01 +0900

    remove : 안쓰는 import 삭제

--- app/(banner-layout)/contact/page.tsx
+++ app/(banner-layout)/contact/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next';
-import Banner from '../../../components/Banner/Banner';
 import EmailForm from '../../../components/Email/EmailForm';
 
 export const metadata: Metadata = {
