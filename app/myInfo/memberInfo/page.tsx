74145a2f96551a4e149932c23b9f188625f9a580
wook <yho7955@naver.com>
2024-09-07 11:09:31 +0900

    style: memberinfo, MyInfo 컴포넌트 반응형 적용

--- app/myInfo/memberInfo/page.tsx
+++ app/myInfo/memberInfo/page.tsx
@@ -44,58 +44,74 @@ export default function MemberInfoPage() {
   }
 
   return (
-    <main className="flex flex-col items-center justify-center h-[70vh] p-4 sm:p-8">
-      <section className="w-full bg-white p-8 rounded shadow-md  max-w-md sm:max-w-2xl">
-        <div className="flex gap-2 justify-center mb-8">
-          <img src="/icon/profile.svg" alt="유저의 프로필 아이콘" className="w-4 sm:w-7 h-auto" />
-          <h1 className="text-3xl font-bold text-center ">회원 정보</h1>
-        </div>
-        {userData?.phoneNumber ? (
-          <div className="flex gap-2">
-            <span className="text-gray-700 font-bold text-lg">전화번호</span>
-            <span className="text-lg">{userData?.phoneNumber}</span>
-            <Button
-              type="button"
-              label="수정"
-              width="auto"
-              fontSize="base"
-              onClick={() => setIsPhoneNumberModalOpen(true)}
-              className="text-white font-bold bg-primary hover:bg-blue-500"
+    <main className="flex flex-col items-center justify-center h-[40rem] p-4 sm:p-8">
+      <section className="w-full bg-white p-8 rounded shadow-lg max-w-md sm:max-w-xl">
+        <div className="flex flex-col items-center mb-4">
+          <div className="w-auto h-auto bg-gray-400 rounded-full p-5 mb-5">
+            <img
+              src="/icon/mypageProfile.svg"
+              alt="나의 정보 프로필 아이콘"
+              className="w-25 h-25"
             />
           </div>
-        ) : (
-          <PhoneNumber />
-        )}
-        {userData ? (
-          <>
-            <div className="flex gap-2">
-              <span className="text-gray-700 font-bold text-lg">우편주소</span>
-              <span className="text-lg">{userData.address.address}</span>
-              <Button
-                type="button"
-                label="수정"
-                width="auto"
-                fontSize="base"
-                onClick={() => setIsAddressModalOpen(true)}
-                className="text-white font-bold bg-primary hover:bg-blue-500"
-              />
+          <h2 className="text-xl font-semibold">{userData?.name}</h2>
+          <div className="flex gap-2">
+            <img src="/icon/email.svg" alt="유저의 이메일 아이콘" className="w-4 sm:w-5 h-auto " />
+            <p className="text-gray-600 text-sm sm:text-base">{userData?.email}</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          {userData?.phoneNumber ? (
+            <div className="flex flex-col items-start gap-1 w-full">
+              <span className="text-gray-700 font-bold text-lg">전화번호</span>
+              <div className="flex justify-between w-full">
+                <span className="text-lg">{userData?.phoneNumber}</span>
+                <Button
+                  type="button"
+                  label="수정"
+                  width="auto"
+                  fontSize="base"
+                  onClick={() => setIsPhoneNumberModalOpen(true)}
+                  className="text-white font-bold bg-primary hover:bg-blue-500"
+                />
+              </div>
             </div>
-            <div className="flex gap-2">
-              <span className="text-gray-700 font-bold text-lg">상세 주소</span>
-              <span className="text-lg">{userData.address.detail}</span>
-            </div>
-          </>
-        ) : (
-          <AddressForm
-            address={address}
-            detailAddress={detailAddress}
-            isPostcodeOpen={isPostcodeOpen}
-            setIsPostcodeOpen={setIsPostcodeOpen}
-            handleComplete={handleComplete}
-            handleOnChange={handleOnChange}
-            handleRegisterAddress={handleRegisterAddress}
-          />
-        )}
+          ) : (
+            <PhoneNumber />
+          )}
+          {userData ? (
+            <>
+              <div className="w-full flex flex-col items-start gap-1 ">
+                <span className="text-gray-700 font-bold text-lg">우편주소</span>
+                <div className="flex justify-between w-full">
+                  <span className="text-lg">{userData.address.address}</span>
+                  <Button
+                    type="button"
+                    label="수정"
+                    width="auto"
+                    fontSize="base"
+                    onClick={() => setIsAddressModalOpen(true)}
+                    className="text-white font-bold bg-primary hover:bg-blue-500"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col items-start gap-1 w-full">
+                <span className="text-gray-700 font-bold text-lg">상세 주소</span>
+                <span className="text-lg">{userData.address.detail}</span>
+              </div>
+            </>
+          ) : (
+            <AddressForm
+              address={address}
+              detailAddress={detailAddress}
+              isPostcodeOpen={isPostcodeOpen}
+              setIsPostcodeOpen={setIsPostcodeOpen}
+              handleComplete={handleComplete}
+              handleOnChange={handleOnChange}
+              handleRegisterAddress={handleRegisterAddress}
+            />
+          )}
+        </div>
       </section>
       {isPhoneNumberModalOpen && (
         <PhoneNumberModal onClose={() => setIsPhoneNumberModalOpen(false)} />
