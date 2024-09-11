348fd7f994cf641971ab24a0e85178447dfce128
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-09-11 14:37:13 +0900

    refactor: 중복되는 커스텀 훅 삭제

--- lib/hooks/useMyInfo.ts
+++ lib/hooks/useMyInfo.ts
@@ -2,6 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { getMyInfo } from '../../api/user/myInfo';
 import useAuthStore from '../store/useAuthStore';
 
+/**
+ * 사용자의 정보를 나타내는 커스텀 훅
+ * @returns 사용자 정보 및 인증토큰 반환
+ */
 export default function useMyInfo() {
   const userToken = useAuthStore((state) => state.userToken);
 
