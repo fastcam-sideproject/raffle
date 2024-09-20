5c85af239b1b8f5a31bc99edb753c202ba88a8d6
wook <yho7955@naver.com>
2024-09-20 17:21:16 +0900

    fix: root page에 불필요한 코드 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-'use client';
 import HomeMain from '../components/Home/HomeMain';
 import HeaderNav from '../components/Header/HeaderNav';
 import AuthHandler from '../components/AuthHandler';
-import useAuthStore from '../lib/store/useAuthStore';
 
 export default function HomePage() {
-  const { userToken } = useAuthStore((state) => ({
-    userToken: state.userToken,
-  }));
-
   return (
     <>
       <HeaderNav />
