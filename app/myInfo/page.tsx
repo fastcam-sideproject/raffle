489a02f59473ab3b957008ff22669e62050c5b58
wook <yho7955@naver.com>
2024-10-04 16:47:38 +0900

    feat: myInfo 페이지에 사용자가 /myInfo URL에 직접 접근하려고 할 때, 인증되지 않은 경우 로그인 페이지로 리디렉션하도록 기능 구현

--- app/myInfo/page.tsx
+++ app/myInfo/page.tsx
@@ -1,13 +1,21 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import MyInfoStyle from '../../components/MyInfoStyle';
 import useMyInfo from '../../lib/hooks/useMyInfo';
 
 export default function MyInfoPage() {
   const { data: userData, isLoading, isError, error } = useMyInfo();
+  const router = useRouter();
 
-  if (isLoading) return <div>Loading...</div>;
+  useEffect(() => {
+    if (!userData) {
+      router.push('/');
+    }
+  }, [userData, router]);
+
+  if (isLoading) return <div>로딩 중...</div>;
 
   if (isError)
     return <div>Error: {error instanceof Error ? error.message : JSON.stringify(error)}</div>;
